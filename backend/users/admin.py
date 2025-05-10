526d03a79ba54f7eebfa0e8d7addc3ebb4b0c1e5
SaveliyKrivov <krivovsaveliy28@gmail.com>
2025-05-10 19:01:39 +0300

    admin pannel done

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Subscription
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
@@ -8,3 +8,8 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ('Дополнительно', {'fields' : ('avatar',)}),
     )
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'following')
+    search_fields = ('user__username', 'following__username')
